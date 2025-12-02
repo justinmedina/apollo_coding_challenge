ad7a22897c414e53fd72bc0b6b464693f2be9d22
Justin Medina <justinmedina@mac.mynetworksettings.com>
2025-12-01 21:13:43 -0500

    Added initial POST and GET endpoints for Vehicle resource with supporting service functions. Tested manually through FastAPI Swagger UI. Pytest coverage will be added in the next commit.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
 from .database import Base, engine, SessionLocal
+from . import services, schemas
 
 app = FastAPI(title="Vehicle Application")
 
@@ -13,3 +16,21 @@ def get_db():
         yield db
     finally:
         db.close()
+
+#endpoints
+        
+# create vehicle
+@app.post("/vehicle",response_model=schemas.VehicleResponse)
+def create_vehicle(vehicle: schemas.VehicleCreate, db:Session = Depends(get_db)):
+    existing_vehicle = services.getVehicle(db, vehicle.vin)
+    if existing_vehicle:
+        raise HTTPException(status_code=400, detail="Vehicle with this VIN already exists")
+    return services.createVehicle(db,vehicle)
+
+# get a vehicle
+@app.get("/vehicle/{vin}",response_model=schemas.VehicleResponse)
+def get_vehicle(vin: str, db:Session = Depends(get_db)):
+    vehicle = services.getVehicle(db, vin)
+    if not vehicle:
+        raise HTTPException(status_code=404, detail="Vehicle not found")
+    return vehicle
---
+++ app/services.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+# CRUD
+# Create
+# Read
+# Update
+# Delete
+
+# Create a vehicle
+# pydantic -> sql alchemy 
+def createVehicle(db:Session, vehicle_data: schemas.VehicleCreate):
+    vehicle = models.Vehicle(
+        vin=vehicle_data.vin,
+        manufacturer=vehicle_data.manufacturer,
+        description=vehicle_data.description,
+        horse_power=vehicle_data.horse_power,
+        model_name=vehicle_data.model_name,
+        model_year=vehicle_data.model_year,
+        purchase_price=vehicle_data.purchase_price,
+        fuel_type=vehicle_data.fuel_type
+    )
+    db.add(vehicle)
+    db.commit()
+    db.refresh(vehicle)
+    return vehicle
+
+
+# get a vehicle by VIN
+def getVehicle(db:Session, vin:str):
+    vehicle = db.query(models.Vehicle).filter(models.Vehicle.vin == vin).first()
+    return vehicle
